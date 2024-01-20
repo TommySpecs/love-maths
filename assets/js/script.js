6098d79d316932129eea939203efd820b21dd8c1
TommySpecs <>
2024-01-20 14:27:08 +0000

    checked code due to game not working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,6 @@
-//Wait for the DOM to finish loading before running the game
-//Get the button elements and add event listener to them
 
-document.addEventListener("DOMContentLoaded", function() {
-let buttons = document.getElementsByTagName("button");
-
-for (let button of buttons) {
-button.addEventListener("click", function(){
-if (this.getAttribute("data-type") === "submit") {
-    alert("You Clicked Submit");
-} else{
-    let gameType = this,getAttribute("data-type");
-    alert('You Clicked $(gameType)');
-}
-})
-}
-})
-
-/**
- * The main game "loop", called when the scrip: is first loaded
- * and after the user 's answer has been processed
- */
-function runGame() {
-    //creates two random numbers between 1 and 25
-    let num1 = Math.floor(math.random() *25) + 1;
-    let num2 = Math.floor(math.random() * 25) + 1;
+function runGame(gameType) {   
+    
 }
 
 
@@ -44,8 +21,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
